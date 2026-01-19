eef6102e8e168062f332502bfcf946bd4f458862
autorepuestos-chasi <autorepuestos.cv.01@gmail.com>
2026-01-19 00:28:34 -0500

    Interfaz móvil optimizada

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# =========================
+# =========================ESTE ULTIMO BUENO
 # CONFIGURACIÓN GENERAL
 # =========================
 st.set_page_config(
