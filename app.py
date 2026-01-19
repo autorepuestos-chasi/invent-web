588b009217e67a8801e9e588c6288b7b78a6f633
autorepuestos-chasi <autorepuestos.cv.01@gmail.com>
2026-01-19 03:37:10 -0500

    Interfaz móvil optimizada

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ st.markdown("<p style='text-align:center;'>INVENTARIO</p>", unsafe_allow_html=Tr
 # =========================
 # CARGA DE DATOS (ANTI CACHE DRIVE)
 # =========================
-URL_BASE = "https://docs.google.com/spreadsheets/d/e/2PACX-XXXX/pub?gid=507673529&single=true&output=csv"
+URL_BASE = "cdffe"
 URL_CSV = f"{URL_BASE}&v={int(time.time() // 60)}"
 CACHE_LOCAL = "cache_datos.csv"
 
