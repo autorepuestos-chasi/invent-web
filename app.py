fe8c97c2445934f01e5150a9b128b224d1688d04
autorepuestos-chasi <autorepuestos.cv.01@gmail.com>
2026-01-19 04:32:20 -0500

    Interfaz móvil optimizada

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if st.button("🔄 Actualizar datos"):
 # =========================
 # CARGA DE DATOS (ENDPOINT ESTABLE)
 # =========================
-URL_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRqvgoLkCTGBXrDPQgs4kIDa8YgZqk0lyMh9vJ8_IiipSRmJJN2kReZzsH8n8YCDg/pub?gid=45200806&single=true&output=csv"
+URL_CSV = "https://docs.google.com/spreadsheets/d/12CJmMjPZ8O9MG0OBq7v9t56i93mUHfzN/export?format=csv&gid=45200806"
 
 @st.cache_data(ttl=600)
 def cargar_datos():
