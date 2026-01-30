e24473ecb2580e079bbc236d3e6266c83f0c87eb
autorepuestos-chasi <autorepuestos.cv.01@gmail.com>
2026-01-30 14:00:38 -0500

    Actualizacion URL CSV

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if "ultima_actualizacion" in st.session_state:
 # =========================
 # LINK CSV PUBLICADO (CORRECTO)
 # =========================
-URL_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjvIAfApdQmXKQavdfz6vYdOmR1scVPOvmW66mgpDMXjMO_EyZcLI9Ezuy8vNkpA/pub?gid=282818568&single=true&output=csv" # AUTO_EDIT
+URL_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRjvIAfApdQmXKQavdfz6vYdOmR1scVPOvmW66mgpDMXjMO_EyZcLI9Ezuy8vNkpA/pub?gid=762765730&single=true&output=csv" # AUTO_EDIT
 
 # =========================
 # BOTÓN ACTUALIZAR (ANTI BUG)
@@ -165,6 +165,7 @@ if busqueda:
         )
     else:
         st.warning("No se encontraron resultados")
+
 
 
 
