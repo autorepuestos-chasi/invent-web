c05ee94befdccf98c72a68e7ac5771d9e0e018e6
autorepuestos-chasi <autorepuestos.cv.01@gmail.com>
2026-02-03 20:43:31 -0500

    Actualizacion URL CSV

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if "ultima_actualizacion" in st.session_state:
 # LINK CSV PUBLICADO (CORRECTO)
 # =========================
 
-URL_CSV = "jkkjk" # AUTO_EDIT
+URL_CSV = "jkji" # AUTO_EDIT
 
 # =========================
 # BOTÓN ACTUALIZAR (ANTI BUG)
@@ -166,6 +166,7 @@ if busqueda:
         )
     else:
         st.warning("No se encontraron resultados")
+
 
 
 
